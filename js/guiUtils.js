3d18cd0b0accd428481bba9ba17cd0893240f454
Fredrik Johnson <frejo989@student.liu.se>
2014-09-29 09:14:34 +0200

    dat gui element at top? gone! bam easý just remove it. so ez

--- js/guiUtils.js
+++ js/guiUtils.js
@@ -2,6 +2,6 @@ function setUpGui() {
 	console.log('set up gui here');
 	guiGroup = game.add.group()
 
-	guiUpper = guiGroup.create(0, 0, 'spriteGuiUpper');
+	//guiUpper = guiGroup.create(0, 0, 'spriteGuiUpper');
 	guiLower = guiGroup.create(0, 877, 'spriteGuiLower' );
 }
