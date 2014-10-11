1fdeb599fba568599cea5db8eee6a82fa2c882b9
Fredrik Johnson <frejo989@student.liu.se>
2014-10-11 14:38:21 +0200

    hotfix for tutorial

--- js/tutorials.js
+++ js/tutorials.js
@@ -30,12 +30,12 @@ function swapTutorials(index) {
         tutorialContinue.inputEnabled = true;
         tutorialContinue.events.onInputDown.add(tutorialListener, this);
         game.world.bringToTop(guiGroup);
-        game.world.bringToTop(gameBoard[4][4].barrelSprite);
+        game.world.bringToTop(gameBoard[winIndex].barrelSprite);
         game.world.bringToTop(tutorial_1_1);
         game.world.bringToTop(tutorialContinue);
-        uppdateBarrelVisibility(2, true);
-        gameBoard[2][4].barrelSprite.inputEnabled = false;
-        gameBoard[2][3].barrelSprite.inputEnabled = false;
+        updateBarrelVisibility2();
+        gameBoard[16].barrelSprite.inputEnabled = false;
+        gameBoard[15].barrelSprite.inputEnabled = false;
         console.log(index);
     } else if(index == 2) {
         //rollUpMenuAnimation(tutorial_1_1, -910, 700);
@@ -47,8 +47,8 @@ function swapTutorials(index) {
         game.world.bringToTop(tintBlack);
         game.world.bringToTop(guiGroup);
         game.world.bringToTop(tutorial_1_2);
-        game.world.bringToTop(gameBoard[2][4].barrelSprite);
-        gameBoard[2][4].barrelSprite.inputEnabled = true;
+        game.world.bringToTop(gameBoard[16].barrelSprite);
+        gameBoard[16].barrelSprite.inputEnabled = true;
         console.log(index);
     } else if(index == 3) {
         //rollUpMenuAnimation(tutorial_1_2, -910, 700);
@@ -58,8 +58,8 @@ function swapTutorials(index) {
         game.world.bringToTop(tintBlack);
         game.world.bringToTop(guiGroup);
         game.world.bringToTop(tutorial_1_3);
-        game.world.bringToTop(gameBoard[2][3].barrelSprite);
-        gameBoard[2][3].barrelSprite.inputEnabled = true;
+        game.world.bringToTop(gameBoard[15].barrelSprite);
+        gameBoard[15].barrelSprite.inputEnabled = true;
         console.log(index);
     } else {
        // rollUpMenuAnimation(tutorial_1_3, -910, 700);
